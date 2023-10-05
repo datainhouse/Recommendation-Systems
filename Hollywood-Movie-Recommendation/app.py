68accd50c388f8789deec69b0cb4225c2e9387ac
datainhouse <146379682+datainhouse@users.noreply.github.com>
2023-10-05 12:56:39 +0530

    Update app.py

--- Hollywood-Movie-Recommendation/app.py
+++ Hollywood-Movie-Recommendation/app.py
@@ -39,7 +39,7 @@ def fetch_poster(movie_id):
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity_matrix[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse = True, key = lambda x:x[1])[1:10]
+    movies_list = sorted(list(enumerate(distances)), reverse = True, key = lambda x:x[1])[1:13]
     recommended = []
     posters = []
     for i in movies_list:
